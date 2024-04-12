bc6639c16d8905916933516c3b5cf34b2fcf993d
ItsSur3 <jpetroruiz@gmail.com>
2024-04-11 19:32:27 -0500

    first commit

---
+++ src/routers/router.js
@@ -0,0 +1,18 @@
+import { Routes } from "../utils/consts.js";
+
+const route = (event) => {
+  event = event || window.event;
+  event.preventDefault();
+  window.history.pushState({}, "", event.target.href);
+  handleLocation();
+};
+
+const handleLocation = async () => {
+  const path = window.location.pathname;
+  const route = Routes[path] || Routes[404];
+  const html = await fetch(route).then((data) => data.text());
+  document.getElementById("main-page").innerHTML = html;
+};
+window.onpopstate = handleLocation;
+window.route = route;
+handleLocation();
---
+++ src/utils/consts.js
@@ -0,0 +1,5 @@
+export const Routes = {
+  404: "/src/pages/404.html",
+  "/": "/src/pages/home.html",
+  "/about": "/src/pages/about.html",
+};
