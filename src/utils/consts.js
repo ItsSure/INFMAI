d1ad5f98ca66a79980f90e77625fd36aa173d411
ItsSur3 <jpetroruiz@gmail.com>
2024-05-04 03:58:36 -0500

    Subo avances

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,5 +1,7 @@
 export const Routes = {
   404: "/src/pages/404.html",
   "/": "/src/pages/home.html",
+  "/explore": "/src/pages/explore.html",
   "/about": "/src/pages/about.html",
+  "/contact": "/src/pages/contact.html",
 };
