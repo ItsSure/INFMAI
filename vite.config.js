9d424c90548cfc64bf041d5ffe08df222ac860d6
ItsSur3 <jpetroruiz@gmail.com>
2024-05-08 03:11:14 -0500

    Conf .env y vite

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,15 @@ import { resolve } from "path";
 export default defineConfig(({ command, mode }) => {
   const port = 3000;
 
-  // console.log(command, mode);
-  // const env = loadEnv(mode, process.cwd());
-  // console.log(env.VITE_NAME);
-
   if (mode === "development") {
-    // console.log("modo desarrollo");
     return {
       server: {
         port,
       },
     };
   } else {
-    // console.log("modo produccion");
     return {
+      base: process.env.VITE_BASE_URL || "/",
       build: {
         rollupOptions: {
           input: {
@@ -27,7 +22,6 @@ export default defineConfig(({ command, mode }) => {
             about: resolve(__dirname, "src", "pages", "about.html"),
             contact: resolve(__dirname, "src", "pages", "contact.html"),
             explore: resolve(__dirname, "src", "pages", "explore.html"),
-            explorejs: resolve(__dirname, "src", "js", "explore.js"),
           },
         },
       },
