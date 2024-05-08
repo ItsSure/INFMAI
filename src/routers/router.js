287c861d34a88a7e318f77e2048d12f689aba030
ItsSur3 <jpetroruiz@gmail.com>
2024-05-08 03:04:42 -0500

    Revert "Solucion rutas en prod"
    This reverts commit 3872922cb2c239a6eb5c51b24403367fe58be20d.

--- src/routers/router.js
+++ src/routers/router.js
@@ -10,20 +10,17 @@ const route = (event) => {
 const handleLocation = async () => {
   const path = window.location.pathname;
   const route = Routes[path] || Routes[404];
-  const basePath = window.location.origin;
-  const resourcePath =
-    path === "/explore" ? "/assets/explorejs-djjztg7f.js" : route;
-  const html = await fetch(basePath + resourcePath).then((data) => data.text());
+  const html = await fetch(route).then((data) => data.text());
   document.getElementById("main-page").innerHTML = html;
 
+  // Agregar el script explore.js manualmente
   if (path === "/explore") {
     const scriptElement = document.createElement("script");
     scriptElement.type = "module";
-    scriptElement.src = basePath + "/src/js/explore.js";
+    scriptElement.src = "/src/js/explore.js";
     document.body.appendChild(scriptElement);
   }
 };
-
 window.onpopstate = handleLocation;
 window.route = route;
 handleLocation();
