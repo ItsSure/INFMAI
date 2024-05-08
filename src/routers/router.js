3b6a294ad34fde6f095ee50b1ee688cf9545a386
ItsSur3 <jpetroruiz@gmail.com>
2024-05-08 02:59:45 -0500

    Probando si se corrige problema de rutas

--- src/routers/router.js
+++ src/routers/router.js
@@ -11,16 +11,29 @@ const handleLocation = async () => {
   const path = window.location.pathname;
   const route = Routes[path] || Routes[404];
   const basePath = window.location.origin;
-  const resourcePath =
-    path === "/explore" ? "/assets/explorejs-djjztg7f.js" : route;
-  const html = await fetch(basePath + resourcePath).then((data) => data.text());
-  document.getElementById("main-page").innerHTML = html;
-
+  let resourcePath = "";
   if (path === "/explore") {
-    const scriptElement = document.createElement("script");
-    scriptElement.type = "module";
-    scriptElement.src = basePath + "/src/js/explore.js";
-    document.body.appendChild(scriptElement);
+    resourcePath = "/assets/explorejs-djjztg7f.js";
+  } else {
+    resourcePath = route.startsWith("/") ? route : "/assets/" + route;
+  }
+
+  try {
+    const response = await fetch(basePath + resourcePath);
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const html = await response.text();
+    document.getElementById("main-page").innerHTML = html;
+
+    if (path === "/explore") {
+      const scriptElement = document.createElement("script");
+      scriptElement.type = "module";
+      scriptElement.src = basePath + "/src/js/explore.js";
+      document.body.appendChild(scriptElement);
+    }
+  } catch (error) {
+    console.error("Error fetching resource:", error);
   }
 };
 
